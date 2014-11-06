a4367cc895b18c5e200cc9f9558692cbdac118a2
David Mandelberg <dmandelb@bbn.com>
2014-11-06 18:50:13 -0500

    implement the hash functions

--- fasguardlib-bloom/mk/libfasguardbloom.mk
+++ fasguardlib-bloom/mk/libfasguardbloom.mk
@@ -10,7 +10,8 @@ LDADD_LIBFASGUARDBLOOM = \
 include_HEADERS += \
 	src/fasguardbloom.hpp
 
-src_libfasguardbloom_la_SOURCES =
+src_libfasguardbloom_la_SOURCES = \
+	src/fasguardbloom.cpp
 
 src_libfasguardbloom_la_LDFLAGS = \
 	-version-info 0:0:0
