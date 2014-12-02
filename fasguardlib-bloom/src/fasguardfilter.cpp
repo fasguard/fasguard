6eb50c471028e61526f38c66c83273be1ceaa89b
David Mandelberg <dmandelb@bbn.com>
2014-12-01 22:00:17 -0500

    major updates to the interface based on conversation with Dan

--- fasguardlib-bloom/src/fasguardfilter.cpp
+++ fasguardlib-bloom/src/fasguardfilter.cpp
@@ -1,4 +1,9 @@
+// Make sure to get enough out of inttypes.h
+#define __STDC_FORMAT_MACROS
+#define __STDC_LIMIT_MACROS
+
 #include <cstdio>
+#include <inttypes.h>
 
 #include <fasguardfilter.hpp>
 
@@ -16,16 +21,19 @@ filter_parameters::~filter_parameters()
 }
 
 
-std::string filter::to_string() const
+std::string filter_statistics::to_string() const
 {
-    static char const format[] = "unknown_filter[%s]";
+    static char const format[] =
+        "default_statistics["
+        "insertions = %" PRIuFAST64 ", "
+        "unique_insertions = %" PRIuFAST64 "]";
+    static size_t const buflen =
+        sizeof(format) +
+        32 /* > digits in PRIuFAST64 */ * 2 /* count of PRIuFAST64 */;
 
-    std::string const & parameters_str = parameters->to_string();
-
-    size_t const buflen = sizeof(format) + parameters_str.length();
     char * buf = new char[buflen];
 
-    snprintf(buf, buflen, format, parameters_str.c_str());
+    snprintf(buf, buflen, format, insertions, unique_insertions);
 
     std::string ret(buf);
 
@@ -34,20 +42,89 @@ std::string filter::to_string() const
     return ret;
 }
 
-bool filter::insert(
-    uint8_t const * data,
-    size_t length)
+filter_statistics::~filter_statistics()
 {
-    bool const ret = contains(data, length);
-    insert_no_test(data, length);
-    return ret;
 }
 
-void filter::insert_no_test(
-    uint8_t const * data,
-    size_t length)
+filter_statistics::filter_statistics()
+:
+    insertions(0),
+    unique_insertions(0)
 {
-    (void)insert(data, length);
+}
+
+void filter_statistics::on_insert(
+    uint8_t const * data,
+    size_t length,
+    bool unique)
+{
+    (void)data;
+    (void)length;
+
+    if (insertions < UINT_FAST64_MAX)
+    {
+        ++insertions;
+    }
+
+    if (unique && unique_insertions < UINT_FAST64_MAX)
+    {
+        ++unique_insertions;
+    }
+}
+
+void filter_statistics::on_insert_all(
+    filter_statistics const * other)
+{
+    if (other == NULL)
+    {
+        return;
+    }
+
+    /**
+        @brief Set <tt>left</tt> to <tt>min(left + right, max)</tt>.
+
+        This is similar to <tt>left += right</tt>, but takes the upper
+        limit of <tt>left</tt> into account.
+    */
+    #define CAPPED_INCREMENT(left, right, max) \
+        do \
+        { \
+            if ((left) >= (max) - (right)) \
+            { \
+                left = (max); \
+            } \
+            else \
+            { \
+                left = (right); \
+            } \
+        } while (false)
+
+    CAPPED_INCREMENT(insertions, other->insertions, UINT_FAST64_MAX);
+
+    // This is potentially wrong, but we don't have enough information
+    // to make it right.
+    CAPPED_INCREMENT(unique_insertions, other->unique_insertions,
+        UINT_FAST64_MAX);
+
+    #undef CAPPED_INCREMENT
+}
+
+void filter_statistics::on_contains(
+    uint8_t const * data,
+    size_t length,
+    bool contains)
+{
+    // For now, don't track this.
+    (void)data;
+    (void)length;
+    (void)contains;
+}
+
+
+std::string filter::to_string() const
+{
+    static std::string const ret("unknown_filter");
+    return ret;
 }
 
 bool filter::insert_all(
@@ -60,6 +137,16 @@ bool filter::insert_all(
 filter::~filter()
 {
     delete parameters;
+    delete statistics;
+}
+
+filter::filter(
+    filter_parameters const * parameters_,
+    filter_statistics * statistics_)
+:
+    parameters(parameters_),
+    statistics(statistics_)
+{
 }
 
 }
--- fasguardlib-bloom/src/fasguardfilter.hpp
+++ fasguardlib-bloom/src/fasguardfilter.hpp
@@ -25,6 +25,117 @@ public:
         @brief Destructor.
     */
     virtual ~filter_parameters();
+
+    friend class filter;
+
+protected:
+    /**
+        @brief Default constructor.
+    */
+    filter_parameters();
+
+private:
+    filter_parameters(
+        filter_parameters const & other);
+
+    filter_parameters & operator=(
+        filter_parameters const & other);
+};
+
+/**
+    @brief Base class for statistics for a filter.
+*/
+class filter_statistics
+{
+public:
+    /**
+        @brief Return a string that describes the statistics.
+
+        E.g., this might be
+        "bloom_filter_statistics[insertions=7, duplicate_insertions=4]"
+        for statistics for a bloom filter.
+    */
+    virtual std::string to_string() const;
+
+    /**
+        @brief Destructor.
+    */
+    virtual ~filter_statistics();
+
+    /**
+        @brief Number of times an attempt was made to insert an item
+            into the filter.
+
+        If this is greater than or equal to UINT64_MAX, further
+        insertions may be counted, but the count may be reset to
+        UINT64_MAX if the filter is saved to disk and loaded back
+        again. If it's is equal to UINT_FAST64_MAX, no further
+        insertions will be counted.
+    */
+    uint_fast64_t insertions;
+
+    /**
+        @brief Number of items inserted into the filter that were not
+            already present in the filter.
+
+        The note in about upper limits in #insertions applies to this
+        field too.
+
+        @note This number may be way too large if two or more filters
+            are merged, e.g., with #filter::insert_all.
+    */
+    uint_fast64_t unique_insertions;
+
+    friend class filter;
+
+protected:
+    /**
+        @brief Default constructor.
+    */
+    filter_statistics();
+
+    /**
+        @brief Callback for #filter::insert.
+
+        @param[in] data Data that was inserted.
+        @param[in] length Length of @p data.
+        @param[in] unique True if the data was (probably) not already
+            present in the filter; false if the data was (probably)
+            already in the filter.
+    */
+    virtual void on_insert(
+        uint8_t const * data,
+        size_t length,
+        bool unique);
+
+    /**
+        @brief Callback for #filter::insert_all.
+
+        @param[in] other Statistics from the other filter. This may be
+            NULL, however these statistics will not be correct if this
+            is NULL.
+    */
+    virtual void on_insert_all(
+        filter_statistics const * other);
+
+    /**
+        @brief Callback for #filter::contains.
+
+        @param[in] data Data that was tested.
+        @param[in] length Length of @p data.
+        @param[in] contains Result of the #filter::contains test.
+    */
+    virtual void on_contains(
+        uint8_t const * data,
+        size_t length,
+        bool contains);
+
+private:
+    filter_statistics(
+        filter_statistics const & other);
+
+    filter_statistics & operator=(
+        filter_statistics const & other);
 };
 
 /**
@@ -36,36 +147,28 @@ public:
     /**
         @brief Return a string that describes the filter.
 
+        This should not include information that's already in the
+        filter's parameters or statistics.
+
         E.g., this might be
-        "bloom_filter[bloom_filter_parameters[bitlength = 1024, num_hashes = 5], bits_set = 123, false_positive_rate = 0.00123]"
+        "bloom_filter[bits_set = 123, false_positive_rate = 0.00123]"
         for a bloom filter.
     */
     virtual std::string to_string() const;
 
     /**
-        @brief Insert data, and return true iff the data was already
-            in the filter.
+        @brief Insert data.
 
-        @note Non-abstract derived classes must implement at least one
-            of this and #insert_no_test.
-
-        @sa insert_no_test
+        @note All implementations of this method should call
+            #filter_statistics::on_insert iff #statistics is not NULL.
+            Derived classes should not call their parent's
+            implementation of this, to avoid duplicate calls to
+            #filter_statistics::on_insert.
     */
-    virtual bool insert(
+    virtual void insert(
         uint8_t const * data,
-        size_t length);
-
-    /**
-        @brief Insert the data, without first testing for membership.
-
-        @note Non-abstract derived classes must implement at least one
-            of this and #insert.
-
-        @sa insert
-    */
-    virtual void insert_no_test(
-        uint8_t const * data,
-        size_t length);
+        size_t length)
+        = 0;
 
     /**
         @brief If possible, add all elements from the other filter
@@ -75,7 +178,16 @@ public:
         other filter into this one and return true. Otherwise, make no
         changes to this filter and return false.
 
+        If a derived class does not override this, the default
+        implementation always returns false.
+
         @note The two filters must be of the same derived class.
+
+        @note All implementations of this method should call
+            #filter_statistics::on_insert_all iff #statistics is not
+            NULL and insertion is possible. Derived classes should
+            not call their parent's implementation of this, to avoid
+            duplicate calls to #filter_statistics::on_insert_all.
     */
     virtual bool insert_all(
         filter const & other);
@@ -84,12 +196,17 @@ public:
         @brief Return true iff the filter (probably) contains the
             data.
 
-        @note Non-abstract derived classes must implement this.
+        @note All implementations of this method should call
+            #filter_statistics::on_contains iff #statistics is not
+            NULL. Derived classes should not call their parent's
+            implementation of this, to avoid duplicate calls to
+            #filter_statistics::on_contains.
     */
     virtual bool contains(
         uint8_t const * data,
         size_t length)
-        const;
+        const
+        = 0;
 
     /**
         @brief Destructor.
@@ -100,11 +217,40 @@ public:
 
     /**
         @brief Parameters for this filter.
-
-        @note This must be initialized by all constructors, including
-            those in derived classes.
     */
     filter_parameters const * const parameters;
+
+    /**
+        @brief Statistics for this filter.
+
+        This may be NULL.
+
+        The statistics may be updated even when <tt>this</tt> is
+        const.
+    */
+    filter_statistics * const statistics;
+
+protected:
+    /**
+        @brief Constructor.
+
+        @param[in] parameters_ Parameters for this filter. This must
+            not be NULL. This is deleted by #~filter.
+        @param[in] statistics_ Default-constructed statistics of the
+            correct derived type. This may be NULL to indicate that
+            tracking of statistics is not desired. This is deleted by
+            #~filter.
+    */
+    filter(
+        filter_parameters const * parameters_,
+        filter_statistics * statistics_);
+
+private:
+    filter(
+        filter const & other);
+
+    filter & operator=(
+        filter const & other);
 };
 
 }
